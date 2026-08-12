e567cb1f84398f5c7b6be8bdead06066296d8321
Husni Adil Makmur <husni.adil@gmail.com>
2026-08-12 21:18:14 +0700

    fix: wire Rust modules through the Tauri library

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-mod paths;
-mod platform;
-
 fn main() {
     claude_profiles_lib::run()
 }
